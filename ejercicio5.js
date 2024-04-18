d4570d9caf717857f9952975d09ea6af22b33068
Alexander <alexanderacosta2286@gmail.com>
2024-04-17 19:21:56 -0500

    ejercicio 5

---
+++ ejercicio5.js
@@ -0,0 +1,14 @@
+const numeroSecreto = Math.floor(Math.random() * 100) + 1;
+let numeroUsuario = -1;
+numeroUsuario = prompt("Ingrese un número entre 1 y 100:");
+
+while (numeroUsuario != numeroSecreto) {
+    if (numeroUsuario < numeroSecreto) {
+        alert("El número secreto es mayor. Inténtalo de nuevo.");
+    } else if (numeroUsuario > numeroSecreto) {
+        alert("El número secreto es menor. Inténtalo de nuevo.");
+    }
+    numeroUsuario = prompt("Ingrese un número entre 1 y 100:");
+}
+
+console.log("¡Felicidades! Has adivinado el número secreto.");
